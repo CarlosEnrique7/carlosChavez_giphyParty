bceed57d121d8d8f9aa4939e47b3952eaccc46c3
vZireael <75550025+vZireael@users.noreply.github.com>
2021-06-09 18:21:25 -0400

    implemeted functionality to load more gif's and allow searching multiple times

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 // global constants
 const apiKey = "api_key=PB1PvDciWiOZc46GmuCoJSXK27seF8YD";
-const limit = "&20";
-const rating = "&g";
+const limit = "&limit=10";
+const rating = "&rating=g";
+let offset = 0;
 
 const form = document.querySelector("form");
 const gifArea = document.querySelector("#gif-area");
+const loadBtn = document.querySelector("#load-btn");
 
-form.addEventListener("submit", async (evt) => {
-  // stop page reload
+let pageNumber = 0;
+
+form.addEventListener("submit", (evt) => {
+  offset = 0;
   evt.preventDefault();
-
-  const value = evt.target.gif.value;
-  const term = "&q=" + value;
-  const apiUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}${limit}${rating}${term}`;
-
-  const response = await fetch(apiUrl);
-  const responseData = await response.json();
-
-  console.log(responseData);
-  displayResults(responseData.data);
+  clearHTML();
+  getResults(evt);
+  loadBtn.classList.remove("hidden");
+  loadBtn.addEventListener("click", () => {
+    offset += 10;
+    getResults(evt);
+    pageNumber++;
+  });
 });
 
 function displayResults(response) {
@@ -30,4 +32,20 @@ function displayResults(response) {
     </div>`;
     index++;
   });
+}
+
+function clearHTML() {
+  gifArea.innerHTML = " ";
+}
+
+async function getResults(evt) {
+  const value = evt.target.gif.value;
+  const term = "&q=" + value;
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}${limit}${rating}${term}&offset=${offset}`;
+
+  const response = await fetch(apiUrl);
+  const responseData = await response.json();
+
+  console.log(responseData);
+  displayResults(responseData.data);
 }
