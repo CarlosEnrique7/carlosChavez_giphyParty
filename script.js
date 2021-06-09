f363362c4002b74f5f045c30f64b5fddecc7b131
vZireael <75550025+vZireael@users.noreply.github.com>
2021-06-09 16:59:00 -0400

    added fetch api and grid of images

--- script.js
+++ script.js
@@ -6,11 +6,28 @@ const rating = "&g";
 const form = document.querySelector("form");
 const gifArea = document.querySelector("#gif-area");
 
-console.log("loaded");
-
-form.addEventListener("submit", (evt) => {
+form.addEventListener("submit", async (evt) => {
   // stop page reload
   evt.preventDefault();
+
   const value = evt.target.gif.value;
-  const apiUrl = `api.giphy.com/v1/gifs/search?${apiKey}${limit}${rating}`;
+  const term = "&q=" + value;
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}${limit}${rating}${term}`;
+
+  const response = await fetch(apiUrl);
+  const responseData = await response.json();
+
+  console.log(responseData);
+  displayResults(responseData.data);
 });
+
+function displayResults(response) {
+  index = 0;
+  response.forEach((element) => {
+    gifArea.innerHTML += `
+    <div class="gif">
+    <img class="gif-image" src="${response[index].images.fixed_height.url}" alt="${response[index].title}"/>
+    </div>`;
+    index++;
+  });
+}
